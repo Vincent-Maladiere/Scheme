68f6d30baae6c53a671fbdbb2b8609c5a85b9b45
Vincent-Maladiere <maladiere.vincent@yahoo.fr>
2016-10-12 17:28:27 +0200

    Update object.c

--- object.c
+++ object.c
@@ -1,4 +1,12 @@
 
+//
+//  object.c
+//  Scheme
+//
+//  Created by Vincent Maladiere on 20/09/16.
+//  Copyright © 2016 Vincent Maladiere. All rights reserved.
+//
+
 /**
  * @file object.c
  * @author François Cayre <cayre@yiking.(null)>
@@ -11,22 +19,31 @@
 #include "object.h"
 #include "mem.h"
 
+object cons( object car, object cdr ) {
+    object t = make_object(SFS_PAIR);
+    
+    t->this.pair.car = car;
+    t->this.pair.cdr = cdr;
+    
+    return t;
+}
 
 object make_object( uint type ) {
-
+    
     object t = sfs_malloc( sizeof( *t ) );
-
+    
     t->type = type;
-
+    
     return t;
 }
 
 object make_nil( void ) {
-
+    
     object t = make_object( SFS_NIL );
-
+    
     t->this.special = t;
-
+    
     return t;
 }
+
 
