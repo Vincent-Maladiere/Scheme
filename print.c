5b1bdb1cad687a1e0afe53b8fb08d231414919e4
Vincent-Maladiere <maladiere.vincent@yahoo.fr>
2016-10-12 17:25:43 +0200

    Update print.c

--- print.c
+++ print.c
@@ -1,4 +1,12 @@
+//
+//  print.c
+//  Scheme
+//
+//  Created by Vincent Maladiere on 05/10/16.
+//  Copyright © 2016 Vincent Maladiere. All rights reserved.
+//
 
+#include <stdio.h>
 /**
  * @file print.c
  * @author François Cayre <cayre@yiking.(null)>
@@ -26,31 +34,69 @@ void sfs_print_atom( object o ) {
         printf("%s",o->this.symbol);
     }
     if(o->type == SFS_BOOLEAN){
-        (o->this.number.this.integer == 1) ? puts("#t") : puts("#f");
+        (o->this.number.this.integer == 0) ? printf("#f") : printf("#t");
     }
+    
     return;
 }
 
-void sfs_print_pair( object o ) {
+void sfs_print_pair_save( object o ) {
     if(o->this.pair.car->type == SFS_PAIR){
-        puts("(");
+        printf("(");
         sfs_print(o->this.pair.car);
     }
-    sfs_print(o->this.pair.car);
-    sfs_print(o->this.pair.cdr);
-    if(o->this.pair.cdr == nil){
-        puts(")");
+    else{
+        sfs_print(o->this.pair.car);
+        if(o->this.pair.cdr != nil){ /*si ce n'est pas le dernière atome, espace entre les atomes*/
+            printf(" ");
+        }
+        if(o->this.pair.cdr == nil){
+            printf(")");
+        }
+        else{
+            sfs_print(o->this.pair.cdr);
+        }
+        
     }
     return;
 }
 
-void sfs_print( object o ) {
+
+void sfs_print_pair_save_old( object o ) {
     
+    if (o) {
+        printf("(");
+        sfs_print(o->this.pair.car);
+        printf(" ");
+        sfs_print_pair(o->this.pair.cdr);
+        printf(")");
+    }
+    else WARNING_MSG("On ne devrait pas etre la");
+    return;
+}
+
+
+void sfs_print_pair( object o ) {
+
+    printf("(");
+    while(o != nil)
+    {
+        sfs_print(o->this.pair.car);
+        o = o->this.pair.cdr;
+        if(o != nil){
+            printf(" ");
+        }
+    }
+    printf(")");
+    return;
+}
+
+
+void sfs_print( object o ) {
     if ( SFS_PAIR == o->type ) {
         sfs_print_pair( o );
     }
     else {
         sfs_print_atom( o );
     }
-    
 }
