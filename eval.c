2909e2ee7a367e3eddc3ad487b7aed58f9806ded
Vincent-Maladiere <maladiere.vincent@yahoo.fr>
2016-10-14 17:35:08 +0200

    Update eval.c

--- eval.c
+++ eval.c
@@ -1,32 +1,19 @@
-//
-//  eval.c
-//  Scheme
-//
-//  Created by Vincent Maladiere on 20/09/16.
-//  Copyright © 2016 Vincent Maladiere. All rights reserved.
-//
 
-/**
- * @file eval.c
- * @author François Cayre <cayre@yiking.(null)>
- * @date Fri Jun 22 20:11:30 2012
- * @brief Evaluation stuff for SFS.
- *
- * Evaluation stuff for SFS.
- */
 
 #include "eval.h"
 
 object sfs_eval( object expr ) {
+    
     restart :
-    if((expr->type==SFS_NUMBER || expr->type==SFS_STRING || expr->type==SFS_BOOLEAN  || expr->type== SFS_PAIR || expr->type==SFS_CHARACTER)){
-        return expr;  /* pas d'eval donc autoevaluant */
+    
+    if((expr->type==SFS_NUMBER || expr->type==SFS_STRING || expr->type==SFS_BOOLEAN  || expr->type== SFS_PAIR|| expr->type==SFS_CHARACTER)){
+        return expr; * /* pas d'eval donc autoevaluant */
     }
-    if(is_form("quote",expr)) {
+    if(is_form("quote",expr) == 0) {
         return expr->this.pair.cdr;
     }
-    if(is_form("if",expr)) {
-        if(true == eval(cdr(car(expr)))){
+    if(is_form("if",expr) == 0) {
+        if(sfs_eval(cdr(car(expr))) == TRUE){
             expr = cdr(cdr(car(expr)));
             goto restart;
         }
@@ -34,15 +21,23 @@ object sfs_eval( object expr ) {
             expr = cdr(cdr(cdr(car(expr))));
             goto restart;
         }
-    expr = caddr
     }
-    
-    
+    if(is_form("define", expr) == 0){
+        
+    }
+    if(is_form("set!", expr) == 0){
+        
+    }
+    if(is_form("or", expr) == 0){
+        
+    }
+    if(is_form("and", expr) == 0){
+        
     }
 }
 
 
-int is_form (char *name, object expr) {   /* detecter si c'est une forme oui==1, 0 sinon */
+ int is_form (char *name, object expr) { /* detecter si c'est une forme oui==1, 0 sinon */
     if ((expr->type == SFS_PAIR) && (expr->this.pair.car->type == SFS_SYMBOL) && (0 == strcmp(name, expr->this.pair.car->this.string))){
         return 1;
     }
@@ -50,4 +45,3 @@ int is_form (char *name, object expr) {   /* detecter si c'est une forme oui==1,
         return 0;
     }
 }
-
