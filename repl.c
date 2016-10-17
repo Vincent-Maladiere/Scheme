cb274932fdf5080f385649af4bb927668b19e3c3
Vincent-Maladiere <maladiere.vincent@yahoo.fr>
2016-10-17 17:14:57 +0200

    Update repl.c

--- repl.c
+++ repl.c
@@ -25,7 +25,7 @@ typedef enum {INTERACTIF,SCRIPT} inter_mode;
 
 
 void usage_error( char *command ) {
-
+    
     fprintf( stderr, "Usage: %s [file.scm]\n   If no file is given, executes in Shell mode.\n", command );
 }
 
@@ -33,34 +33,34 @@ void usage_error( char *command ) {
 object nil;
 
 void init_interpreter ( void ) {
-
+    
     nil      = make_nil();
-
+    
 }
 
 int main ( int argc, char *argv[] ) {
-
+    
     char     input[BIGSTRING];
     uint     here = 0;
     object   output = NULL;
     object   sexpr = NULL;
     inter_mode mode;
     FILE *   fp = NULL; /* le flux dans lequel les commande seront lues : stdin (mode shell) ou un fichier */
-
+    
     /* exemples d'utilisation des macros du fichier notify.h */
     /* WARNING_MSG : sera toujours affiche */
     WARNING_MSG("Un message WARNING_MSG !");
-
+    
     /* macro INFO_MSG : uniquement si compil avec -DVERBOSE. Cf Makefile*/
     INFO_MSG("Un message INFO_MSG : Debut du programme %s", argv[0]);
-
+    
     /* macro DEBUG_MSG : uniquement si compil avec -DDEBUG (ie : compil avec make debug). Cf Makefile */
     DEBUG_MSG("Un message DEBUG_MSG !");
-
+    
     /* La ligne suivante provoquerait l'affichage du message
-       puis la sortie du programme avec un code erreur non nul (EXIT_FAILURE) */
+     puis la sortie du programme avec un code erreur non nul (EXIT_FAILURE) */
     /* ERROR_MSG("Erreur. Arret du programme"); */
-
+    
     if ( argc > 2 ) {
         usage_error( argv[0] );
         exit( EXIT_FAILURE );
@@ -69,9 +69,9 @@ int main ( int argc, char *argv[] ) {
         usage_error( argv[0] );
         exit( EXIT_SUCCESS );
     }
-
+    
     init_interpreter();
-
+    
     /*par defaut : mode shell interactif */
     fp = stdin;
     mode = INTERACTIF;
@@ -84,17 +84,17 @@ int main ( int argc, char *argv[] ) {
         }
         mode = SCRIPT;
     }
-
-
+    
+    
     while ( 1 ) {
         input[0]='\0';
         here = 0;
         output = NULL;
         sexpr = NULL;
         uint Sexpr_err;
-
+        
         Sexpr_err = sfs_get_sexpr( input, fp );
-
+        
         if ( S_OK != Sexpr_err) {
             /* si fichier alors on sort*/
             if (mode == SCRIPT) {
@@ -109,19 +109,19 @@ int main ( int argc, char *argv[] ) {
             /*sinon on rend la main à l'utilisateur*/
             continue;
         }
-
+        
         if ( 0 == strlen( input ) ) {
             continue;
         }
-
+        
         /* Pour le step 0 on se contente d'afficher */
         /* la S-Expression lue */
         /* puis d'en lire une autre */
         /* METTRE EN COMMENTAIRE LES DEUX LIGNES SUIVANTES */
         /* POUR PASSER A L'INCREMENT 1 */
-        printf("%s\n", input );
-        continue;
-
+        /* printf("%s\n", input ); */
+        /* continue; */
+        
         here  = 0;
         sexpr = sfs_read( input, &here );
         if ( NULL == sexpr ) {
@@ -134,7 +134,7 @@ int main ( int argc, char *argv[] ) {
             /*sinon on rend la main à l'utilisateur*/
             continue ;
         }
-
+        
         output = sfs_eval( sexpr );
         if( NULL == output) {
             /* si fichier alors on sort*/
@@ -146,13 +146,18 @@ int main ( int argc, char *argv[] ) {
             /*sinon on rend la main à l'utilisateur*/
             continue ;
         }
-
+        
         printf( "==> " );
-        sfs_print( output );
+        if(output == nil){
+            printf("()");
+        }
+        else{
+            sfs_print( output );
+        }
         printf( "\n" );
     }
-
-    if (mode == SCRIPT) {
+    
+    if ((mode == SCRIPT)) {
         fclose( fp );
     }
     exit( EXIT_SUCCESS );
