14c6c82c600c9014bdbc1f1e06a0b139292bcf1f
pierrotbat <pierrotbat@gmail.com>
2016-11-05 14:24:58 +0100

    Update primitive.c

--- primitive.c
+++ primitive.c
@@ -2,7 +2,10 @@ object plus (object A, object B)
 // si le type de A et B sont bien des integer, on puet les aditionner, sinon: erreur
 {
     if ((A->this->type == SFS_NUMBER) || (B->type->type == SFS_NUMBER)) {
-        res= (A->this.number.this.integer) + (B->this.number.this.integer); }
+        object res = make_object(0);
+        res= (A->this.number.this.integer) + (B->this.number.this.integer);
+        return res;
+        }
     else {
     return ERROR_MSG("\n plus impossible\n"); }
 }
