773dc1edd0aa48816b3ac1b30ade84e447de15c6
Vincent-Maladiere <maladiere.vincent@yahoo.fr>
2016-11-17 11:20:22 +0100

    Update primitive.c

--- primitive.c
+++ primitive.c
@@ -1,12 +1,79 @@
-object *plus_p (object A, object B)
-// si le type de A et B sont bien des integer, on puet les aditionner, sinon: erreur
-{
-    if ((A->this->type == SFS_NUMBER) || (B->type->type == SFS_NUMBER)) {
-        object * res = NULL;
-        * res = make_object(0);
-        res= (A->this.number.this.integer) + (B->this.number.this.integer);
-        return *res;
+#include "primitive.h"
+
+object make_primitive(void){
+    list_prim = make_object(SFS_PAIR);
+    list_prim->this.pair.car = NULL;
+    list_prim->this.pair.cdr = make_object(SFS_PAIR);
+    
+    object o = make_object(SFS_PAIR);
+    o = list_prim->this.pair.cdr;
+    
+    /* arithmétique */
+    
+    /* + */
+    
+    o->this.pair.car = make_object(SFS_PAIR);
+    o->this.pair.car->this.pair.car = make_object(SFS_SYMBOL);
+    char str5[256] = {'+'};
+    strcpy(o->this.pair.car->this.pair.car->this.symbol,str5);
+    
+    o->this.pair.car->this.pair.cdr = make_object(SFS_PRIMITIVE);
+    o->this.pair.car->this.pair.cdr->this.primitive.fonction = plus_p;
+    
+    o->this.pair.cdr = make_object(SFS_PAIR);
+    o = o->this.pair.cdr;
+    
+    /* - */
+    
+    o->this.pair.car = make_object(SFS_PAIR);
+    o->this.pair.car->this.pair.car = make_object(SFS_SYMBOL);
+    char str6[256] = {'-'};
+    strcpy(o->this.pair.car->this.pair.car->this.symbol,str6);
+    
+    o->this.pair.car->this.pair.cdr = make_object(SFS_PRIMITIVE);
+    o->this.pair.car->this.pair.cdr->this.primitive.fonction = moins_p;
+    
+    o->this.pair.cdr = make_object(SFS_PAIR);
+    o = o->this.pair.cdr;
+
+    
+    
+
+    
+    return list_prim;
+}
+
+
+
+object plus_p (object arbreAAditionner){
+    int res = 0;
+    while(arbreAAditionner->this.pair.car != NULL){
+        if(arbreAAditionner->this.pair.car->type == SFS_NUMBER){
+            res += arbreAAditionner->this.pair.car->this.number.this.integer;
         }
-    else {
-    ERROR_MSG("\n plus impossible\n"); }
+        else{
+            ERROR_MSG("\n Il y a un non entier dans l'arbre argument");
+        }
+        arbreAAditionner = arbreAAditionner->this.pair.cdr;
+    }
+    object resObject = make_object(SFS_NUMBER);
+    resObject->this.number.this.integer = res;
+    return resObject;
+}
+
+object moins_p (object arbreASoustraire){
+    int res = arbreASoustraire->this.pair.car->this.number.this.integer;
+    arbreASoustraire = arbreASoustraire->this.pair.cdr;
+    while(arbreASoustraire->this.pair.car != NULL){
+        if(arbreASoustraire->this.pair.car->type == SFS_NUMBER){
+            res -= arbreASoustraire->this.pair.car->this.number.this.integer;
+        }
+        else{
+            ERROR_MSG("\n Il y a un non entier dans l'arbre argument");
+        }
+        arbreASoustraire = arbreASoustraire->this.pair.cdr;
+    }
+    object resObject = make_object(SFS_NUMBER);
+    resObject->this.number.this.integer = res;
+    return resObject;
 }
