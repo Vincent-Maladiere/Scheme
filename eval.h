19e55a35cdcce5c2e74182b71e122cf03533bb90
Vincent-Maladiere <maladiere.vincent@yahoo.fr>
2016-12-13 13:23:32 +0100

    Update eval.h

--- eval.h
+++ eval.h
@@ -1,3 +1,14 @@
+
+
+/**
+ * @file eval.h
+ * @author François Cayre <cayre@yiking.(null)>
+ * @date Fri Jun 22 20:10:38 2012
+ * @brief Evaluation stuff for SFS.
+ *
+ * Evaluation stuff for SFS.
+ */
+
 #ifndef _EVAL_H_
 #define _EVAL_H_
 
@@ -22,8 +33,9 @@ extern "C" {
     object make_lambda(object expr);
     object lambda_a_evaluer(object expr, object env_lambda);
     object lambda_a_evaluer_apres_define(object expr, object env_lambda);
+    object conversion_fonction_lambda(object expr);
+    object execution_primitive(object expr, object prim_object, object env);
+    
 #ifdef __cplusplus
 }
 #endif
-
-#endif /* _EVAL_H_ */
